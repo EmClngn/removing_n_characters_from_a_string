6d0ad3b9caaaa81da0e410db95398adde4dbea74
unknown <emgcalangian@gmail.com>
2024-01-22 13:34:58 +0800

    added len function for the string length

--- removing_n_characters_from_a_string.py
+++ removing_n_characters_from_a_string.py
@@ -7,7 +7,8 @@
 # asking users for a string and int respectively
 string = input("What is your desried word?: ")
 number_of_characters = input("What's the number of characters you want to be removed in the string? ")
-
+string_length = len(string)
+print(string_length)
 # write the program that will remove the chracters from the string
 for i in range(0, 4):
     print(string[i])
