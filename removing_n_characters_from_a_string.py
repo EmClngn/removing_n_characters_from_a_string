dc82199a7a1eaea34ee34cc1ad7012675d1c496b
unknown <emgcalangian@gmail.com>
2024-01-22 13:32:30 +0800

    added for i in range

--- removing_n_characters_from_a_string.py
+++ removing_n_characters_from_a_string.py
@@ -7,7 +7,7 @@
 # asking users for a string and int respectively
 string = input("What is your desried word?: ")
 number_of_characters = input("What's the number of characters you want to be removed in the string? ")
-print(string)
-print(number_of_characters)
 
 # write the program that will remove the chracters from the string
+for i in range(0, 4):
+    print(string[i])
