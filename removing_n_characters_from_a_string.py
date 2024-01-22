84783e0524cb58c7d89dd5d027f15542822ba0c0
unknown <emgcalangian@gmail.com>
2024-01-22 13:23:37 +0800

    pseudo code

---
+++ removing_n_characters_from_a_string.py
@@ -0,0 +1,6 @@
+# Write a program to remove characters from a string starting from zero up to n and return a new string.
+
+# Pseudo Code
+
+# asking users for a string and int respectively
+# write the program that will remove the chracters from the string
