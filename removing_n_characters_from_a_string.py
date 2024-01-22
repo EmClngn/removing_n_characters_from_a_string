1a28f2f9ea3f76271f858a73ceaf5cc006415af1
unknown <emgcalangian@gmail.com>
2024-01-22 13:41:26 +0800

    program that removes characters from a string is written

--- removing_n_characters_from_a_string.py
+++ removing_n_characters_from_a_string.py
@@ -6,9 +6,9 @@
 
 # asking users for a string and int respectively
 string = input("What is your desried word?: ")
-number_of_characters = input("What's the number of characters you want to be removed in the string? ")
+number_of_characters = int(input("What's the number of characters you want to be removed in the string? "))
 string_length = len(string)
-print(string_length)
+
 # write the program that will remove the chracters from the string
-for i in range(0, 4):
+for i in range(number_of_characters, string_length):
     print(string[i])
