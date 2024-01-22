eb0ae9bb4a3cee5cabaf5857bda7de90a6441c6e
unknown <emgcalangian@gmail.com>
2024-01-22 13:28:02 +0800

    user input for string and number

--- removing_n_characters_from_a_string.py
+++ removing_n_characters_from_a_string.py
@@ -1,6 +1,13 @@
 # Write a program to remove characters from a string starting from zero up to n and return a new string.
 
+
+
 # Pseudo Code
 
 # asking users for a string and int respectively
+string = input("What is your desried word?: ")
+number_of_characters = input("What's the number of characters you want to be removed in the string? ")
+print(string)
+print(number_of_characters)
+
 # write the program that will remove the chracters from the string
